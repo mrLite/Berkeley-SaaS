2e8a5bd9cfcf6d5e71aa3b0088c86b5330ce3156
AQ <aku.peltonen@gmail.com>
2012-03-27 21:24:58 +0300

    Final versions

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -8,3 +8,10 @@ def combine_anagrams(words)
   end
   groups.uniq
 end
+
+# def combine_anagrams(words)
+#   anagram_list = {}
+#   words.each {|word| anagram_list[word.downcase.split(//).sort.join] = [] unless anagram_list.has_key? word.downcase.split(//).sort.join }
+#   words.each {|w| anagram_list[w.downcase.split(//).sort.join] << w }
+#   anagram_list.collect {|k,v| v}
+# end
--- hw2/part1.rb
+++ hw2/part1.rb
@@ -6,7 +6,24 @@ end
 
 module Enumerable
   def palindrome?
-    self.each_index { |i| return false unless self[i] == self[-i-1] }
-    true
+    self.to_a == self.to_a.reverse
+  end
+end
+
+class Numeric
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0}
+  def method_missing(method_id)
+    singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self * @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+  
+  def in(currency)
+    singular_currency = currency.to_s.gsub(/s$/,'')
+    raise "Don't know the currency #{currency}" unless @@currencies.has_key?(singular_currency)
+    self / (@@currencies[singular_currency])
   end
 end
